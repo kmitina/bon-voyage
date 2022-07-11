061ef8e20130047960bc146105c8c9b3135a41ea
Kris Mitina <kristmitina@gmail.com>
2022-07-11 20:56:30 +0200

    Filling In The Details

--- bon-voyage/VacationDetailsVC.swift
+++ bon-voyage/VacationDetailsVC.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class VacationDetailsVC: UIViewController {
     
+    @IBOutlet weak var activitiesLbl: UILabel!
+    @IBOutlet weak var descriptionLbl: UILabel!
+
+    
     var vacation: Vacation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = vacation.title
+        
+        activitiesLbl.text = vacation.activities
+        descriptionLbl.text = vacation.description
 
     }
 
